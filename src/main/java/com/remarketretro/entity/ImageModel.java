4a297cddd16b523bde757e5be84c38e79382561a
Mariele Santos <marielecsantos@outlook.com>
2024-05-22 21:41:32 -0300

    adiciona possibilidade de cadastrar imagens dos produtos

---
+++ src/main/java/com/remarketretro/entity/ImageModel.java
@@ -0,0 +1,34 @@
+package com.remarketretro.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "image_model")
+@NoArgsConstructor
+@Getter
+@Setter
+public class ImageModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String type;
+    @Column(length = 50000000)
+    private byte[] picByte;
+
+    public ImageModel(String name, String type, byte[] picByte) {
+        this.name = name;
+        this.type = type;
+        this.picByte = picByte;
+    }
+}
--- src/main/java/com/remarketretro/entity/Product.java
+++ src/main/java/com/remarketretro/entity/Product.java
@@ -3,10 +3,16 @@ package com.remarketretro.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -20,4 +26,15 @@ public class Product {
     private String productDescription;
     private Double productDiscountedPrice;
     private Double productActualPrice;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "product_images",
+        joinColumns = {
+            @JoinColumn(name = "product_id")
+        },
+        inverseJoinColumns = {
+            @JoinColumn(name = "image_id")
+        }
+    )
+    private Set<ImageModel> productImages;
 }
